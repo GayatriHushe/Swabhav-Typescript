f7834e3dacd0f6337093fb0a95e270beca1ea516
Gayatri Hushe <68203396+GayatriHushe@users.noreply.github.com>
2021-12-27 09:38:49 +0530

    FYI

--- testInterface/testInterface.ts
+++ testInterface/testInterface.ts
@@ -23,3 +23,16 @@ printInfo([{id:101,firstName:'abc',lastname:'xxy'},
 {id:102,firstName:'def',lastname:'yhs'},
 {id:103,firstName:'pqr',lastname:'ksk',location:'mumbai'},
 ])
+
+
+var arr = [3, 5, 7];
+arr.foo = "hello";
+    
+for (var i in arr) {
+  console.log(i); // logs "0", "1", "2", "foo"
+}
+    
+for (var i of arr) {
+  console.log(i); // logs "3", "5", "7"
+  // it doesn't log "3", "5", "7", "hello"
+}
